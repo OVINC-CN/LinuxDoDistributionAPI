9abc936a62408c04322145b889dba751cd36be7d
U8F69 <orenzhang@ovinc.cn>
2025-05-23 23:34:35 +0800

    feat(vcd): add admin page

---
+++ apps/vcd/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+from apps.vcd.models import (
+    ReceiveHistory,
+    UserReceiveStats,
+    UserShareStats,
+    VirtualContent,
+    VirtualContentItem,
+)
+
+
+@admin.register(VirtualContent)
+class VirtualContentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "allowed_trust_levels", "start_time", "end_time", "created_by"]
+    search_fields = ["name"]
+
+
+@admin.register(VirtualContentItem)
+class VirtualContentItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "virtual_content"]
+
+
+@admin.register(ReceiveHistory)
+class ReceiveHistoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "virtual_content_item", "receiver", "received_at", "client_ip"]
+    list_filter = ["virtual_content", "receiver", "client_ip"]
+
+
+@admin.register(UserReceiveStats)
+class UserReceiveStatsAdmin(admin.ModelAdmin):
+    list_display = ["user", "count"]
+
+
+@admin.register(UserShareStats)
+class UserShareStatsAdmin(admin.ModelAdmin):
+    list_display = ["user", "count"]
